1c94e4b25eb0643e3fd42856957d6941a6eb9ddf
jasonchilcott <jasonchilcottcode@gmail.com>
2020-11-30 18:15:41 -0500

    made matches sort in descending order of compatibility

--- src/Components/MatchCard.js
+++ src/Components/MatchCard.js
@@ -1,40 +1,28 @@
-import React from "react"
-import { NavLink } from 'react-router-dom'
-
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 class MatchCard extends React.Component {
+  state = {} ;
 
-  state = {
-    
-    
-  }
-
-  
-
-  
   render() {
-    
-    
-    return(
-        <>
-        {!this.props.match.id ? <h1>Loading...</h1> : 
-        
-                <div className="match-card" key={this.props.match.id}>
-
-                  <NavLink  to={`/users/${this.props.match.id}`} user={this.props.user} >
-
-                <h4>{this.props.match.username} </h4>
-                  </NavLink>
-        
-
-                  
-                    
-                </div>
-        }
-              </>
-            )
-          }
-        
+    return (
+      <>
+        {!this.props.match.id ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div className="match-card" key={this.props.match.id}>
+            <NavLink
+              to={`/users/${this.props.match.id}`}
+              user={this.props.user}
+            >
+              <h2>
+                {Math.round(this.props.match.compatibility)}% with {this.props.match.username}
+              </h2>
+            </NavLink>
+          </div>
+        )}
+      </>
+    );
+  }
 }
-export default MatchCard
+export default MatchCard;
--- src/Containers/MatchesContainer.js
+++ src/Containers/MatchesContainer.js
@@ -1,83 +1,74 @@
-import React from "react"
-import MatchCard from "../Components/MatchCard"
+import React from "react";
+import MatchCard from "../Components/MatchCard";
 
-class MatchesContainer extends React.Component{
+class MatchesContainer extends React.Component {
+  renderMatches = () => {
+    let matches = this.props.matches;
+    let sortedMatches = [];
 
+    matches.forEach(
+      (match) => (match.compatibility = this.ratingsCompatibility(match))
+    );
 
-  renderMatches = () => this.props.matches.map((match) => {
-    let matchCompatibility = this.ratingsCompatibility(match)
-    return <MatchCard key={match.id} match={match} compatibility={matchCompatibility} user={this.props.user}/>
-  })
+    sortedMatches = matches.sort(
+      (a, b) => parseFloat(b.compatibility) - parseFloat(a.compatibility)
+    );
 
-  
-  
+    return sortedMatches.map((match) => {
+      console.log(match);
+      return <MatchCard key={match.id} match={match} user={this.props.user} />;
+    });
 
-  
+  };
+
   //averages weighted difference of ratings of current user and match and returns compatibility as a percentage
   ratingsCompatibility = (match) => {
-    
-    const user = this.props.user
+    const user = this.props.user;
 
     // array of movie_ids of all movies rated by the current user
-    const currentUserMovies = user.ratings.map((rating) => rating.movie_id)
-    
+    const currentUserMovies = user.ratings.map((rating) => rating.movie_id);
+
     //array of ratings for movies the match and current user have both rated
-    let sharedMovies = match.ratings.filter(rating => currentUserMovies.includes(rating.movie_id))
-    
+    let sharedMovies = match.ratings.filter((rating) =>
+      currentUserMovies.includes(rating.movie_id)
+    );
 
-    //maps through all shared movies, comparing the stars rating the user and match have given 
-    let compareRatings = (
+    //maps through all shared movies, comparing the stars rating the user and match have given
+    let compareRatings = sharedMovies.map((matchRating) => {
+      // the current user's rating for the movie the match rated
+      let userRating = user.ratings.find(
+        (rating) => rating.movie_id === matchRating.movie_id
+      );
 
-      sharedMovies.map(matchRating => {
+      //finds difference between two 0.5-5 ratings and applies an exponential curve
+      let weightedDifference = (a, b) => {
+        return ((a - b) / 4.5) ** 2;
+      };
 
-        // the current user's rating for the movie the match rated
-        let userRating = user.ratings.find(rating => (rating.movie_id === matchRating.movie_id))
+      return weightedDifference(userRating.stars, matchRating.stars);
+    });
 
-        //finds difference between two 0.5-5 ratings and applies an exponential curve
-        let weightedDifference = (a, b) => {
-          return (((a - b)/4.5)**2)
-        } 
-
-      return weightedDifference(userRating.stars, matchRating.stars)
-    
-
-    }))
-
-  
-    
     //margin of error based on how many of the same movie/show/etc two people have both rated
-    let highestPossibleMatch = ((1 - ( 1 / (sharedMovies.length) )) * 100 )
-    
-    let  ratingsDiffArray = compareRatings
+    let highestPossibleMatch = (1 - 1 / sharedMovies.length) * 100;
+
+    let ratingsDiffArray = compareRatings;
 
     //the mean of the weighted difference values of each shared rated movie between user/match, converted to a percentage
-    let compatibility = ((1 - (ratingsDiffArray.reduce((a, b) => a + b) / ratingsDiffArray.length) ) * 100 )
+    let compatibility =
+      (1 - ratingsDiffArray.reduce((a, b) => a + b) / ratingsDiffArray.length) *
+      100;
 
     //returns margin of error if margin of error is greater than match percentage
     if (compatibility > highestPossibleMatch) {
-      return highestPossibleMatch
+      return highestPossibleMatch;
     } else {
-      return compatibility
+      return compatibility;
     }
-
-  }
-
-
-
+  };
 
   render() {
-
-    return(
-        <div className="matches-container">
-            {this.renderMatches()}
-        </div>
-
-    )
-
+    return <div className="matches-container">{this.renderMatches()}</div>;
   }
-
-
-
 }
 
-export default MatchesContainer
+export default MatchesContainer;
