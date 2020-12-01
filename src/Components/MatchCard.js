8a76cf345ad319096bf359c6419eb9744c28b590
jasonchilcott <jasonchilcottcode@gmail.com>
2020-12-01 13:28:48 -0500

    color-coded match percentages

--- src/Components/MatchCard.js
+++ src/Components/MatchCard.js
@@ -4,7 +4,23 @@ import { NavLink } from "react-router-dom";
 class MatchCard extends React.Component {
   state = {} ;
 
+  colorCompatibility = () => {
+    if (this.props.match.compatibility >= 75){
+      return <h2 className='good-match'>{parseFloat(this.props.match.compatibility).toFixed(1)}%</h2>
+    } else if (this.props.match.compatibility <= 50) {
+      return <h2 className='bad-match'>{parseFloat(this.props.match.compatibility).toFixed(1)}%</h2>
+    } else {
+      return <h2 className='okay-match'>{parseFloat(this.props.match.compatibility).toFixed(1)}%</h2>
+    }
+  }
+
   render() {
+    let avatar_url = "no_poster.png"
+    if (this.props.match) {
+      if (this.props.match.avatar_url) {
+        avatar_url = this.props.match.avatar_url
+      }
+    }
     return (
       <>
         {!this.props.match.id ? (
@@ -15,8 +31,12 @@ class MatchCard extends React.Component {
               to={`/users/${this.props.match.id}`}
               user={this.props.user}
             >
+              <img className="match-avatar" src={`${avatar_url}`} alt={`${this.props.match.username}'s avatar`}/>
+
+              {this.colorCompatibility()}
+
               <h2>
-                {parseFloat(this.props.match.compatibility).toFixed(1)}% with {this.props.match.username}
+                {this.props.match.username}
               </h2>
             </NavLink>
           </div>
