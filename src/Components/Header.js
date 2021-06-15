998afa9d8b16d9f81dcf39da9ae49d97e6a7f66a
jasonchilcott <jasonchilcottcode@gmail.com>
2021-06-15 18:13:55 -0400

    changed header logo link to react navlink

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,17 @@
 import Navbar from "./Navbar"
+import { NavLink } from "react-router-dom";
 import React from "react"
 
 const Header = (props) => {
 
     return (
     <header className="header">
-        <a href="/">
-        <img className="logo" src="/logo512.png" alt="watchmatch logo"/>
-        </a>
+    
+        <NavLink to="/" user={props.user}>
+            <img className="logo" src="/logo512.png" alt="watchmatch logo"/>
+        </NavLink>
+
+
         <Navbar user={props.user} logMeOut={props.logMeOut}/>
     </header>
     )
